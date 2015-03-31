f2ce830a15d10c31003acdc81c8984315bb92214
Casey Heuscher <heuscher@gmail.com>
2015-03-31 04:48:13 -0600

    remove misc crap

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -44,7 +44,6 @@ angular.module('originateApp.directives', [])
 
             // Main function to retrieve mouse x-y pos.s
             function getMouseXY(e) {
-                console.info('mousexy fire');
                 if (IE) {
                     // grab the x-y pos.s if browser is IE
                     tempX = event.clientX + document.body.scrollLeft;
@@ -120,22 +119,6 @@ angular.module('originateApp.directives', [])
             scope.parallaxObj = ParallaxService.get();
             ParallaxService.add(element, 0, introHeight, 0.05);
 
-            // $rootScope.$watch('person.id', function (newValue) {
-            //     if (newValue !== undefined && newValue !== null) {
-            //         $scope.products = [];
-            //         AzureAPI.product.query({
-            //             person: $rootScope.person.id,
-            //             limit: 9,
-            //         }).$promise.then(function(products) {
-            //             products.forEach(function(product) {
-            //                 $scope.products.push(new AzureProduct(product.code));
-            //             });
-            //         }, function(error) {
-            //             alertService.add('warning', 'Failed to ' + error.config.method + ' products (' + error.status + ': ' + error.statusText + ')');
-            //         });
-            //     }
-            // });
-
             // Detect if the browser is IE or not.
             // If it is not IE, we assume that the browser is NS.
             var IE = document.all ? true : false;
@@ -149,7 +132,6 @@ angular.module('originateApp.directives', [])
 
             // Main function to retrieve mouse x-y pos.s
             function getMouseXY(e) {
-                console.info('mousexy fire');
                 if (IE) {
                     // grab the x-y pos.s if browser is IE
                     tempX = event.clientX + document.body.scrollLeft;
@@ -168,35 +150,7 @@ angular.module('originateApp.directives', [])
                 return true;
             }
 
-            // function fillstarfield() {
-            //     var bgDiv = element[0].querySelector('#starfield-bg');
-            //     var bgX = 0;
-            //     var bgY = 0;
-            //     var bgFactor = 0.25; //parallax shift factor, the bigger the value, the more it shift for parallax movement
-            //     var bgArray = [];
-            //     bgArray.push(bgDiv, bgX, bgY, bgFactor);
-            //     scope.parallaxObject.push(bgArray);
-
-            //     var introDiv = element[0].querySelector('#intro');
-            //     var introX = 0;
-            //     var introY = (scope.windowHeight / 2) - (angular.element(introDiv)[0].clientHeight / 2);
-            //     var introFactor = 0.05;
-            //     var introArray = [];
-            //     introArray.push(introDiv, introX, introY, introFactor);
-            //     scope.parallaxObject.push(introArray);
-            // }
-
-            // function positionDivs() {
-            //     for (var i=0;i<scope.parallaxObject.length;i++) {
-            //         angular.element(scope.parallaxObject[i][0]).css('left', scope.parallaxObject[i][1] + 'px');
-            //         angular.element(scope.parallaxObject[i][0]).css('top', scope.parallaxObject[i][2] + 'px');
-            //     }
-            // }
-
             $document.on('mousemove', getMouseXY);
-
-            // fillstarfield();
-            // positionDivs();
 
             function moveDiv(tempX, tempY) {
                 for (var i=0;i<scope.parallaxObj.length;i++) {
